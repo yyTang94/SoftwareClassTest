8dbc5ab579453ec2d36b6ed98392dea9346baf29
yyTang94 <yyTang94@foxmail.com>
2017-09-04 10:27:43 +0800

    Create pytest.py

---
+++ pytest.py
@@ -0,0 +1,9 @@
+class Test(object):
+    def __init__(self):
+        pass
+    def print_something(self):
+        print("Hello world")
+
+if __name__ == "__main__":
+    test = Test()
+    test.print_something()
